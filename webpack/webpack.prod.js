804197beaa30705a77934646604c9f2c112c8306
Mukul Jain <jainmukul1996@gmail.com>
2020-06-07 17:29:17 +0530

    chore: build assests

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,9 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
-    mode: 'production'
+  mode: "production",
+  plugins: [...common.plugins, new BundleAnalyzerPlugin()],
 });
