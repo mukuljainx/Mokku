8a57760226be689b6e2e3172ab719d1beb696059
Mukul Jain <jainmukul1996@gmail.com>
2020-08-29 22:25:23 +0530

    feat(example): add body & header to html view

--- example/index.js
+++ example/index.js
@@ -1,22 +1,41 @@
+printToScreen = (response) => {
+  document.querySelector("#body").textContent = JSON.stringify(
+    response.data,
+    undefined,
+    4
+  );
+  document.querySelector("#headers").textContent = JSON.stringify(
+    response.headers,
+    undefined,
+    4
+  );
+};
+
 function sendAPI() {
   axios
     .get("http://demo5468585.mockable.io/patients/empi/goals")
-    .then(({ data }) => {
-      console.log(data);
+    .then((response) => {
+      printToScreen(response);
+      console.log(response.data);
+      console.log(response.headers);
     });
 }
 
 function sendAPI2() {
-  axios.get("https://demo5468585.mockable.io/events").then(({ data }) => {
-    console.log(data);
+  axios.get("https://demo5468585.mockable.io/events").then((response) => {
+    printToScreen(response);
+    console.log(response.data);
+    console.log(response.headers);
   });
 }
 
 function queryParam() {
   axios
     .get("https://demo5468585.mockable.io/events?name=mokku")
-    .then(({ data }) => {
-      console.log(data);
+    .then((response) => {
+      printToScreen(response);
+      console.log(response.data);
+      console.log(response.headers);
     });
 }
 
@@ -25,7 +44,9 @@ function postReq() {
     .post("https://demo5468585.mockable.io/events", {
       message: "hello world",
     })
-    .then(({ data }) => {
-      console.log(data);
+    .then((response) => {
+      printToScreen(response);
+      console.log(response.data);
+      console.log(response.headers);
     });
 }
