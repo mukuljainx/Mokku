14e061a27c1b08f29b374a36d603d8534030c0bb
Hawtim <hawtimzhang@gmail.com>
2022-04-13 00:36:11 +0800

    feat: update dist

--- src/services/message/index.ts
+++ src/services/message/index.ts
@@ -5,7 +5,7 @@ import { IEventMessage } from "../../interface/message";
  *     -> Content Script
  *
  * Content script is bridge between panel and inject for communication
- * as it has both windows event listern and chrome runtime message Listener
+ * as it has both windows event listener and chrome runtime message listener
  * Content Script
  *     -> Panel
  *     -> Hook
