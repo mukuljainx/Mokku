eb2ed6db0d83be84ae45e9a548ced12fa2ee6414
Mukul Jain <jainmukul1996@gmail.com>
2024-05-17 23:58:05 +0530

    chore: build assets

--- dist/js/devtool.js
+++ dist/js/devtool.js
@@ -1,12 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!************************!*\
-  !*** ./src/devtool.ts ***!
-  \************************/
-chrome.devtools.panels.create("Mokku", "icon.png", "panel.html", function (panel) {
-    // code invoked on panel creation
-});
-
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGV2dG9vbC5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQSxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vTW9ra3UvLi9zcmMvZGV2dG9vbC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjaHJvbWUuZGV2dG9vbHMucGFuZWxzLmNyZWF0ZShcIk1va2t1XCIsIFwiaWNvbi5wbmdcIiwgXCJwYW5lbC5odG1sXCIsIGZ1bmN0aW9uIChwYW5lbCkge1xuICAgIC8vIGNvZGUgaW52b2tlZCBvbiBwYW5lbCBjcmVhdGlvblxufSk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+chrome.devtools.panels.create("Mokku","icon.png","panel.html",(function(e){}));
