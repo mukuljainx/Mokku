8ff1b3063a9f9b8cc827591a2b2f3c44f5be6d5d
Mukul Jain <jainmukul1996@gmail.com>
2024-05-21 21:48:15 +0530

    feat(ads): update link and text for chatplayground

--- src/external/chatPlayGround.js
+++ src/external/chatPlayGround.js
@@ -1,11 +1,11 @@
 function createBanner() {
-  const banner = document.createElement('div');
+  const banner = document.createElement("div");
 
   banner.innerHTML = `
     <a
       id="chatplayground-banner"
       style="display: flex;align-items: center;width: 350px;height: fit-content;padding: 10px;background-color: #fff;border: 1px solid #ccc;color: #222;border-radius: 7px;box-shadow: 1px 1px 5px #e1e1e1;transform: scale(1.15);"
-      href="https://chatplayground.ai"
+      href="https://www.chatplayground.ai/mokku"
       target="_blank"
     >
       <div>
@@ -15,7 +15,7 @@ function createBanner() {
       <div>
         <p style="margin: 0; font-size: 0.8rem;font-weight: 800;">ChatPlayground.ai</p>
         <p style="margin: 0;color: #555;">
-          Compare Multiple AI Chatbots for the Best Response
+        Achieve Better AI Answers 73% of the Time with Multiple AI Chatbots
 
           <span style="color: rgb(50, 104, 206);margin-left: 5px;text-decoration: underline;">Get Started</span>
         </p>
@@ -24,17 +24,21 @@ function createBanner() {
   `;
 
   // Append the banner to the body
-  if(document.querySelector(".kp-wholepage.kp-wholepage-osrp.HSryR.EyBRub")) {
+  if (document.querySelector(".kp-wholepage.kp-wholepage-osrp.HSryR.EyBRub")) {
     banner.style.marginBottom = "1.5rem";
-    return document.querySelector(".kp-wholepage.kp-wholepage-osrp.HSryR.EyBRub").prepend(banner);
+    return document
+      .querySelector(".kp-wholepage.kp-wholepage-osrp.HSryR.EyBRub")
+      .prepend(banner);
   }
 
-  if(document.querySelector(".TzHB6b.Hwkikb.WY0eLb.LMRCfc.EqrSYc")) {
+  if (document.querySelector(".TzHB6b.Hwkikb.WY0eLb.LMRCfc.EqrSYc")) {
     banner.style.marginBottom = "1.5rem";
-    return document.querySelector(".TzHB6b.Hwkikb.WY0eLb.LMRCfc.EqrSYc").prepend(banner);
+    return document
+      .querySelector(".TzHB6b.Hwkikb.WY0eLb.LMRCfc.EqrSYc")
+      .prepend(banner);
   }
 
-  if(document.querySelector(".osrp-blk")) {
+  if (document.querySelector(".osrp-blk")) {
     banner.style.marginBottom = "1.5rem";
     banner.style.display = "flex";
     banner.style.justifyContent = "center";
@@ -42,14 +46,13 @@ function createBanner() {
     return document.querySelector(".osrp-blk").prepend(banner);
   }
 
-  if(document.querySelector(".TQc1id.k5T88b"))
+  if (document.querySelector(".TQc1id.k5T88b"))
     return document.querySelector(".TQc1id.k5T88b").appendChild(banner);
 
-  if(document.querySelector(".GyAeWb")) {
+  if (document.querySelector(".GyAeWb")) {
     banner.style.marginTop = "1rem";
     return document.querySelector(".GyAeWb").appendChild(banner);
   }
-
 }
 
 createBanner();
